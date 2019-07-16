0329d46086e2388dd864567f2f5432419a0eba5d
“Kimberly <kimberly.achkio@gmail.com>
2019-07-16 16:36:23 -0600

    Add header styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import './Header.css';
 
 export default function Header() {
   return (
     <div>
-      <ul>
-        <li>Shop</li>
-        <li>Read</li>
-        <li>Visit</li>
-        <li>Search</li>
-        <li>Login</li>
-        <li>Cart</li>
-      </ul>
+      <section className="navigation">
+        <ul className="navigation-links-1">
+          <li className="navigation-link-1">Shop</li>
+          <li className="navigation-link-1">Read</li>
+          <li className="navigation-link-1">Visit</li>
+          <li className="navigation-link-1">Search</li>
+        </ul>
+        <ul className="navigation-links-2">
+          <li className="navigation-link-2">Login</li>
+          <li className="navigation-link-2">Cart</li>
+        </ul>
+      </section>
+      
       <h1>Aēsop</h1>
       <h2>Protection in direct and incidental sunlight</h2>
       <p>Introducing Avail Facial Lotion with Sunscreen, to hydrate and help protext the face with broad-spectrum SPF25 coverage</p>
