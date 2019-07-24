c11457310102f87651c9f246d15c269060cc7579
“Kimberly <kimberly.achkio@gmail.com>
2019-07-24 10:39:35 -0600

    Add remaining products

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ParsleySeedSerum from '../../assets/ParsleySeedSerum.png';
 import InTwoMindsFacialToner from '../../assets/InTwoMindsFacialToner.png';
+import ParsleySeedSerum from '../../assets/ParsleySeedSerum.png';
 import MandarinFacialHydratingCream from '../../assets/MandarinFacialHydratingCream.png';
 import PurifyingFacialExfoliantPaste from '../../assets/PurifyingFacialExfoliantPaste.png';
+
+import AmazingFaceCleanser from '../../assets/AmazingFaceCleanser.png';
+import AvailBodyLotion from '../../assets/AvailBodyLotion.png';
+import AvailFacialLotion from '../../assets/AvailFacialLotion.png';
+import BitterOrangeAstringentToner from '../../assets/BitterOrangeAstringentToner.png';
 
 export default function Products() {
   return(
@@ -10,13 +15,13 @@ export default function Products() {
       <section className="products-carousel carousel-1">
         <img
           className="product-image"
-          src={ParsleySeedSerum}
-          alt='dropper of Parsley Seed Serum for Eyes'
+          src={InTwoMindsFacialToner}
+          alt="bottle of In Two Minds Facial Toner"
         />
         <img
           className="product-image"
-          src={InTwoMindsFacialToner}
-          alt="bottle of In Two Minds Facial Toner"
+          src={ParsleySeedSerum}
+          alt='dropper of Parsley Seed Serum for Eyes'
         />
         <img
           className="product-image"
@@ -30,7 +35,37 @@ export default function Products() {
         />
       </section>
       <section className="products-carousel carousel-2">
-        <p>Products</p>
+        <article className="carousel-product-intro">
+          <h4>Seasonal skin care</h4>
+          <h3>Summertime rituals</h3>
+          <p className="carousel-product-intro-description">
+            With the sun's embrace comes the need to adapt our skincare rituals.
+            Warmer weather necessitates thorough cleansing, intermittent exfoliation, and lightweight hydration to leave skin refreshed and revived.
+          </p>
+          <h4>Browse selections</h4>
+        </article>
+        <article className="carousel-2-product-images">
+          <img
+            className="product-image"
+            src={AmazingFaceCleanser}
+            alt="bottle of Amazing Face Cleanser"
+          />
+          <img
+            className="product-image"
+            src={AvailBodyLotion}
+            alt="bottle of Avail Body Lotion"
+          />
+          <img
+            className="product-image"
+            src={AvailFacialLotion}
+            alt="bottle of Avail Facial Lotion"
+          />
+          <img
+            className="product-image"
+            src={BitterOrangeAstringentToner}
+            alt="bottle of Bitter Orange Astringent Toner"
+          />
+        </article>
       </section>
     </div>
   )
