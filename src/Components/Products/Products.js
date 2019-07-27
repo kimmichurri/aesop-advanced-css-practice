9656131d5dd9ebab063247e8226851b7f6f24974
“Kimberly <kimberly.achkio@gmail.com>
2019-07-26 22:55:55 -0600

    Add product descriptions on image hover

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -20,41 +20,83 @@ export default function Products() {
     <div className="products-display-wrapper">
       <section className="products-carousel carousel-1">
         <article className="carousel carousel-1-product-images">
-          <img
-            className="product-image"
-            src={InTwoMindsFacialToner}
-            alt="bottle of In Two Minds Facial Toner"
-          />
-          <img
-            className="product-image"
-            src={ParsleySeedSerum}
-            alt='dropper of Parsley Seed Serum for Eyes'
-          />
-          <img
-            className="product-image"
-            src={MandarinFacialHydratingCream}
-            alt="small tub of Mandarin Facial Hydrating Cream"
-          />
-          <img
-            className="product-image"
-            src={PurifyingFacialExfoliantPaste}
-            alt="tube of Purifying Facial Exfoliant Paste"
-          />
-          <img
-            className="product-image"
-            src={AromatiqueRoomSpray}
-            alt="bottle of Aromatique Room Spray"
-          />
-          <img
-            className="product-image"
-            src={DepartureTravelSet}
-            alt="picture of various products in Departure Travel Set"
-          />
-          <img
-            className="product-image"
-            src={ResurrectionRinseFree}
-            alt="bottle of Resurrection Rinse Free wash"
-          />
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={InTwoMindsFacialToner}
+              alt="bottle of In Two Minds Facial Toner"
+            />
+            <div className="product-image-description">
+              <h4>In Two Minds Facial Cleanser</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={ParsleySeedSerum}
+              alt='dropper of Parsley Seed Serum for Eyes'
+            />
+            <div className="product-image-description">
+              <h4>Parsley Seed Serum</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={MandarinFacialHydratingCream}
+              alt="small tub of Mandarin Facial Hydrating Cream"
+            />
+            <div className="product-image-description">
+              <h4>Mandarin Facial Hydrating Cream</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={PurifyingFacialExfoliantPaste}
+              alt="tube of Purifying Facial Exfoliant Paste"
+            />
+            <div className="product-image-description">
+              <h4>Purifying Facial Exfoliant Paste</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={AromatiqueRoomSpray}
+              alt="bottle of Aromatique Room Spray"
+            />
+            <div className="product-image-description">
+              <h4>Aromatique Room Spray</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={DepartureTravelSet}
+              alt="picture of various products in Departure Travel Set"
+            />
+            <div className="product-image-description box-set-image">
+              <h4>Departure Travel Set</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={ResurrectionRinseFree}
+              alt="bottle of Resurrection Rinse Free wash"
+            />
+            <div className="product-image-description">
+              <h4>Resurrection Rinse Free</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
           </article>
       </section>
       <section className="products-carousel carousel-2">
@@ -68,36 +110,72 @@ export default function Products() {
           <h4>Browse selections</h4>
         </article>
         <article className="carousel carousel-2-product-images">
-          <img
-            className="product-image"
-            src={AmazingFaceCleanser}
-            alt="bottle of Amazing Face Cleanser"
-          />
-          <img
-            className="product-image"
-            src={AvailBodyLotion}
-            alt="bottle of Avail Body Lotion"
-          />
-          <img
-            className="product-image"
-            src={AvailFacialLotion}
-            alt="bottle of Avail Facial Lotion"
-          />
-          <img
-            className="product-image"
-            src={BitterOrangeAstringentToner}
-            alt="bottle of Bitter Orange Astringent Toner"
-          />
-          <img
-            className="product-image"
-            src={NashvilleProductLine}
-            alt="picture of various products from Nashville Product Line"
-          />
-          <img
-            className="product-image"
-            src={ResurrectionDuetHandBalm}
-            alt="two bottles of Resurrection Duet Hand Balm"
-          />
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={NashvilleProductLine}
+              alt="picture of various products from Nashville Product Line"
+            />
+            <div className="product-image-description box-set-image">
+              <h4>Nashville Product Line</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={AmazingFaceCleanser}
+              alt="bottle of Amazing Face Cleanser"
+            />
+            <div className="product-image-description">
+              <h4>Amazing Face Cleanser</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={AvailBodyLotion}
+              alt="bottle of Avail Body Lotion"
+            />
+            <div className="product-image-description">
+              <h4>Avail Body Lotion</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={AvailFacialLotion}
+              alt="bottle of Avail Facial Lotion"
+            />
+            <div className="product-image-description">
+              <h4>Avail Facial Lotion</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={BitterOrangeAstringentToner}
+              alt="bottle of Bitter Orange Astringent Toner"
+            />
+            <div className="product-image-description">
+              <h4>Bitter Orange Astringent Toner</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
+          <div className="product-wrapper">
+            <img
+              className="product-image"
+              src={ResurrectionDuetHandBalm}
+              alt="two bottles of Resurrection Duet Hand Balm"
+            />
+            <div className="product-image-description box-set-image">
+              <h4>Resurrection Duet Hand Balm</h4>
+              <p>A gentle gel-based formulation.</p>
+            </div>
+          </div>
         </article>
       </section>
     </div>
