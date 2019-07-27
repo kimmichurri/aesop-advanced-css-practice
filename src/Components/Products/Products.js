7d07bbf2054449c96ce32537e145c3f25b9fca4d
“Kimberly <kimberly.achkio@gmail.com>
2019-07-26 19:36:39 -0600

    Add more assets

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -4,11 +4,16 @@ import InTwoMindsFacialToner from '../../assets/InTwoMindsFacialToner.png';
 import ParsleySeedSerum from '../../assets/ParsleySeedSerum.png';
 import MandarinFacialHydratingCream from '../../assets/MandarinFacialHydratingCream.png';
 import PurifyingFacialExfoliantPaste from '../../assets/PurifyingFacialExfoliantPaste.png';
+import AromatiqueRoomSpray from '../../assets/AromatiqueRoomSpray.png';
+import DepartureTravelSet from '../../assets/DepartureTravelSet.png';
+import ResurrectionRinseFree from '../../assets/ResurrectionRinseFree.png';
 
 import AmazingFaceCleanser from '../../assets/AmazingFaceCleanser.png';
 import AvailBodyLotion from '../../assets/AvailBodyLotion.png';
 import AvailFacialLotion from '../../assets/AvailFacialLotion.png';
 import BitterOrangeAstringentToner from '../../assets/BitterOrangeAstringentToner.png';
+import NashvilleProductLine from '../../assets/NashvilleProductLine.png';
+import ResurrectionDuetHandBalm from '../../assets/ResurrectionDuetHandBalm.png';
 
 export default function Products() {
   return(
@@ -19,22 +24,37 @@ export default function Products() {
             className="product-image"
             src={InTwoMindsFacialToner}
             alt="bottle of In Two Minds Facial Toner"
-            />
+          />
           <img
             className="product-image"
             src={ParsleySeedSerum}
             alt='dropper of Parsley Seed Serum for Eyes'
-            />
+          />
           <img
             className="product-image"
             src={MandarinFacialHydratingCream}
             alt="small tub of Mandarin Facial Hydrating Cream"
-            />
+          />
           <img
             className="product-image"
             src={PurifyingFacialExfoliantPaste}
             alt="tube of Purifying Facial Exfoliant Paste"
-            />
+          />
+          <img
+            className="product-image"
+            src={AromatiqueRoomSpray}
+            alt="bottle of Aromatique Room Spray"
+          />
+          <img
+            className="product-image"
+            src={DepartureTravelSet}
+            alt="picture of various products in Departure Travel Set"
+          />
+          <img
+            className="product-image"
+            src={ResurrectionRinseFree}
+            alt="bottle of Resurrection Rinse Free wash"
+          />
           </article>
       </section>
       <section className="products-carousel carousel-2">
@@ -67,6 +87,16 @@ export default function Products() {
             className="product-image"
             src={BitterOrangeAstringentToner}
             alt="bottle of Bitter Orange Astringent Toner"
+          />
+          <img
+            className="product-image"
+            src={NashvilleProductLine}
+            alt="picture of various products from Nashville Product Line"
+          />
+          <img
+            className="product-image"
+            src={ResurrectionDuetHandBalm}
+            alt="two bottles of Resurrection Duet Hand Balm"
           />
         </article>
       </section>
