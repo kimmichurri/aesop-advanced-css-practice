44cf1ae3027d542901f5c57ea575028fbc242fa1
“Kimberly <kimberly.achkio@gmail.com>
2019-08-04 21:44:03 -0600

    Style locator component and add image

--- src/Components/Locator/Locator.js
+++ src/Components/Locator/Locator.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './Locator.css';
+import StoreLocatorImage from '../../assets/StoreLocatorImage.png';
 
 export default function Locator() {
   return (
     <div>
-      <p>Visit</p>
-      <p>Store Locator</p>
-      <p>In each store, trained consultants will be pleased to host you.</p>
-      <button>Find a nearby store</button>
+      <article className="store-locator-information">
+        <p className="locator-text">Visit</p>
+        <p className="locator-text">Store Locator</p>
+        <p>In each store, trained consultants will be pleased to host you.</p>
+        <button className="store-locator-button">Find a nearby store</button>
+      </article>
+      <article className="store-locator-image">
+        <img
+          className="featured-store-image"
+          src={StoreLocatorImage}
+          alt="Store in Fremont, CA"
+        />
+      </article>
     </div>
   )
 }
